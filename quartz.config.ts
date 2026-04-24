4b715c0885b897257e14f2350e0af14e9c764a0d
2140787 <m168a@hotmail.it>
2026-04-24 22:42:18 +0200

    fix base url

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "AgazioMaida.github.io/University-notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
